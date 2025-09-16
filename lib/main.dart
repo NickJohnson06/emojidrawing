78de3bb23e5248c517d7d14a6ff38c040e659451
rishitaido <rishitaido@gmail.com>
2025-09-16 19:17:49 -0400

    Deleted unused classes

--- lib/main.dart
+++ lib/main.dart
@@ -90,206 +90,6 @@ class _ShapesDemoScreenState extends State<ShapesDemoScreen> {
   }
 }
 
-class BasicShapesPainter extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {
-    final centerX = size.width / 2;
-    final centerY = size.height / 2;
-    final squareOffset = Offset(centerX - 80, centerY);
-    final circleOffset = Offset(centerX, centerY);
-    final arcOffset = Offset(centerX + 80, centerY);
-    final rectOffset = Offset(centerX - 160, centerY);
-    final lineStart = Offset(centerX - 200, centerY - 50);
-    final lineEnd = Offset(centerX - 140, centerY + 50);
-    final ovalOffset = Offset(centerX + 160, centerY);
-
-    final squarePaint = Paint()..color = Colors.blue;
-    canvas.drawRect(
-      Rect.fromCenter(center: squareOffset, width: 60, height: 60),
-      squarePaint,
-    );
-
-    final circlePaint = Paint()..color = Colors.red;
-    canvas.drawCircle(circleOffset, 30, circlePaint);
-
-    final arcPaint = Paint()
-      ..color = Colors.green
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = 5;
-    canvas.drawArc(
-      Rect.fromCenter(center: arcOffset, width: 60, height: 60),
-      0,
-      2.1,
-      false,
-      arcPaint,
-    );
-
-    final rectPaint = Paint()..color = Colors.orange;
-    canvas.drawRect(
-      Rect.fromCenter(center: rectOffset, width: 80, height: 40),
-      rectPaint,
-    );
-
-    final linePaint = Paint()
-      ..color = Colors.purple
-      ..strokeWidth = 3;
-    canvas.drawLine(lineStart, lineEnd, linePaint);
-
-    final ovalPaint = Paint()..color = Colors.teal;
-    canvas.drawOval(
-      Rect.fromCenter(center: ovalOffset, width: 80, height: 40),
-      ovalPaint,
-    );
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
-}
-
-class CombinedShapesPainter extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {
-    final centerX = size.width / 2;
-    final centerY = size.height / 2;
-
-    final backgroundGradient = RadialGradient(
-      center: Alignment.center,
-      radius: 0.8,
-      colors: [const Color.fromARGB(255, 0, 0, 0), Colors.white],
-    );
-    canvas.drawRect(
-      Rect.fromLTWH(0, 0, size.width, size.height),
-      Paint()
-        ..shader = backgroundGradient.createShader(
-          Rect.fromLTWH(0, 0, size.width, size.height),
-        ),
-    );
-
-    final sunPaint = Paint()..color = Colors.yellow;
-    canvas.drawCircle(Offset(centerX, centerY - 40), 40, sunPaint);
-
-    final rayPaint = Paint()
-      ..color = Colors.yellow
-      ..strokeWidth = 3;
-    for (int i = 0; i < 8; i++) {
-      final angle = i * (pi / 4);
-      final dx = cos(angle) * 60;
-      final dy = sin(angle) * 60;
-      canvas.drawLine(
-        Offset(centerX, centerY - 40),
-        Offset(centerX + dx, centerY - 40 + dy),
-        rayPaint,
-      );
-    }
-
-    final housePaint = Paint()..color = Colors.brown;
-    canvas.drawRect(
-      Rect.fromCenter(center: Offset(centerX, centerY + 40), width: 80, height: 80),
-      housePaint,
-    );
-
-    final roofPaint = Paint()..color = Colors.red;
-    final roofPath = Path()
-      ..moveTo(centerX - 60, centerY)
-      ..lineTo(centerX + 60, centerY)
-      ..lineTo(centerX, centerY - 60)
-      ..close();
-    canvas.drawPath(roofPath, roofPaint);
-
-    final doorPaint = Paint()..color = Colors.blueGrey;
-    canvas.drawRect(
-      Rect.fromCenter(center: Offset(centerX, centerY + 60), width: 30, height: 50),
-      doorPaint,
-    );
-
-    final windowPaint = Paint()..color = Colors.blue.shade200;
-    canvas.drawRect(
-      Rect.fromCenter(center: Offset(centerX - 25, centerY + 20), width: 20, height: 20),
-      windowPaint,
-    );
-    canvas.drawRect(
-      Rect.fromCenter(center: Offset(centerX + 25, centerY + 20), width: 20, height: 20),
-      windowPaint,
-    );
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
-}
-
-class StyledShapesPainter extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {
-    final centerX = size.width / 2;
-    final centerY = size.height / 2;
-
-    final rectGradient = const LinearGradient(
-      begin: Alignment.centerLeft,
-      end: Alignment.centerRight,
-      colors: [Colors.red, Colors.blue],
-    );
-    final rect =
-        Rect.fromCenter(center: Offset(centerX, centerY - 100), width: 200, height: 60);
-    canvas.drawRect(
-      rect,
-      Paint()..shader = rectGradient.createShader(rect),
-    );
-
-    final circlePaint = Paint()..color = Colors.green;
-    final circleBorderPaint = Paint()
-      ..color = Colors.black
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = 4;
-    canvas.drawCircle(Offset(centerX - 80, centerY), 40, circlePaint);
-    canvas.drawCircle(Offset(centerX - 80, centerY), 40, circleBorderPaint);
-
-    final ovalPaint = Paint()..color = Colors.purple.withOpacity(0.5);
-    canvas.drawOval(
-      Rect.fromCenter(center: Offset(centerX + 80, centerY), width: 100, height: 60),
-      ovalPaint,
-    );
-
-    final dashPaint = Paint()
-      ..color = Colors.orange
-      ..strokeWidth = 3
-      ..style = PaintingStyle.stroke;
-
-    final path = Path();
-    double startX = centerX - 100;
-    const dashLength = 10.0;
-    const spaceLength = 5.0;
-    while (startX < centerX + 100) {
-      path.moveTo(startX, centerY + 80);
-      path.lineTo(min(startX + dashLength, centerX + 100), centerY + 80);
-      startX += dashLength + spaceLength;
-    }
-    canvas.drawPath(path, dashPaint);
-
-    final arcGradient = const SweepGradient(
-      center: Alignment.centerRight,
-      startAngle: 0,
-      endAngle: pi,
-      colors: [Colors.red, Colors.yellow, Colors.green],
-    );
-    final arcRect =
-        Rect.fromCenter(center: Offset(centerX, centerY + 100), width: 120, height: 120);
-    final arcPaint = Paint()
-      ..shader = arcGradient.createShader(arcRect)
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = 10
-      ..strokeCap = StrokeCap.round;
-    canvas.drawArc(
-      Rect.fromCenter(center: Offset(centerX, centerY + 100), width: 100, height: 100),
-      0,
-      2.5,
-      false,
-      arcPaint,
-    );
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
-}
 
 class HeartPainter extends CustomPainter {
   @override
@@ -300,16 +100,17 @@ class HeartPainter extends CustomPainter {
     final centerX = size.width / 2;
     final centerY = size.height / 2;
 
+
     path.moveTo(centerX, centerY + 40);
     path.cubicTo(
-      centerX - 60, centerY - 20,
-      centerX - 40, centerY - 80,
-      centerX,       centerY - 40,
+      centerX - 60 , centerY - 20  ,
+      centerX - 40 , centerY - 80,
+      centerX, centerY - 40,
     );
     path.cubicTo(
-      centerX + 40, centerY - 80,
-      centerX + 60, centerY - 20,
-      centerX,       centerY + 40,
+      centerX + 40 , centerY - 80 ,
+      centerX + 60 , centerY - 20 ,
+      centerX, centerY + 40 ,
     );
 
     canvas.drawPath(path, paint);
